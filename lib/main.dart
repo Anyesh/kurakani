e0b1584c0065c7757b6c2a62f3431ada23ebf724
anYesh Shrestha <sthacruz@gmail.com>
2019-09-18 13:46:09 +0545

    initials

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Kurakani());
+
+class Kurakani extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Kurakani");
+  }
+}
